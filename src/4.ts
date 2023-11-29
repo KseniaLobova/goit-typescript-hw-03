13d6ea814200dec2b7ee15fb71def14cf5c9497d
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-11-29 15:33:08 +0100

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+
+class Key{
+    private signature: number
+    constructor( ) {
+        this.signature = Math.random()
+    }
+    getSignature(): number{
+    return this.signature
+    }
+}
+
+class Person {
+    private key:Key
+    constructor(key: Key) {
+        this.key
+    }
+    getKey(): Key{
+        return this.key
+    }
+}
+
+abstract class House{
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+    constructor(key: Key) {
+        this.door = false;
+        this.key=key;
+            
+    }
+
+    comeIn(person: Person):void {
+        if (this.door) {
+            this.tenants.push(person)
+            console.log("Door is open, you can come inside")
+        } else {
+            console.log("Please open the door")
+      }
+  }
+    abstract openDoor(key: Key):void
+}
+
+
+class MyHouse extends House{
+     openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log('Door is open');
+    } else {
+      console.log('Sorry, wrong key, door is close');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
